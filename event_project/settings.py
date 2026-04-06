987ac26d6e32b94d0f43a3efa9850b9395be9115
dishantmali <malidishant23@gmail.com>
2026-04-06 15:57:35 +0530

    commit

--- event_project/settings.py
+++ event_project/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-im@3y9-xgxw9ty2m&=h7(yrpr#3j0msocm*)n5$e_hp^n7t)de'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-im@3y9-xgxw9ty2m&=h7(yrpr#3j0msocm*)n5$e_hp^n7t)de')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['event-manager.onrender.com', 'localhost']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,6 +120,7 @@ LOGOUT_REDIRECT_URL = 'landing'
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 import os
 
 MEDIA_URL = '/media/'
